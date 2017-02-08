361837d6aecbb33bc9e072a886e703786a4dcb14
shruti.deshmukh@accenture.com <ubuntu@ip-10-13-14-153.ap-southeast-1.compute.internal>
2017-02-08 08:39:30 +0000

    modified Hello World java code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World");
+		System.out.println("A message other than Hello World to test code changes");
 	}
 } 
